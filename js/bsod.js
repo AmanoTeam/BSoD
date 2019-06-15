e4e0dac9fcc2717ae52e328ed7c0587aa12bce34
Alisson Lauffer <alissonvitortc@gmail.com>
2019-06-15 17:01:31 -0300

    Some changes

--- js/bsod.js
+++ js/bsod.js
@@ -1,8 +1,7 @@
 var qrcode = new QRCode(document.getElementById("qrcode"), {
 	text: "https://beta.amanotem.ml",
-	width: 120,
-	height: 120,
-	margin: 50,
+	width: 110,
+	height: 110,
 	colorDark : "#106faa",
 	colorLight : "#ffffff",
 	correctLevel : QRCode.CorrectLevel.H
